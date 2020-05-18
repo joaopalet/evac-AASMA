26abaf85667d75bb83c32e691cc19b7b55b3d182
João Palet <joao.palet@outlook.com>
2020-05-18 23:34:29 +0100

    first version with bfs working

---
+++ main.py
@@ -0,0 +1,111 @@
+import pygame
+from settings import *
+from sprites import *
+
+
+
+# Auxiliary Functions
+
+def draw_grid():
+    for x in range(0, WIDTH, TILESIZE):
+        pygame.draw.line(SCREEN, BLACK, (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT, TILESIZE):
+        pygame.draw.line(SCREEN, BLACK, (0, y), (WIDTH, y))
+
+def get_layout():
+    f = open('maze.txt', 'r').read()
+    p = []
+    p = [item.split() for item in f.split('\n')[:-1]]
+    return p
+
+def createWalls():
+    for i in range(int(GRIDWIDTH)):
+        for j in range(int(GRIDHEIGHT)):
+            if layout[i][j] == 'W':
+                wall = Wall(i,j)
+                all_sprites.add(wall)
+                all_walls.add(wall)
+
+
+
+# Main
+
+if __name__ == "__main__":
+    global SCREEN, CLOCK, layout, all_sprites, all_players, all_walls
+
+    pygame.init()
+    pygame.display.set_caption("Evacuation Simulation")
+    pygame.key.set_repeat(200, 100)
+    SCREEN = pygame.display.set_mode((HEIGHT, WIDTH))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+
+    # Create walls and agents
+    layout = get_layout()
+    all_sprites = pygame.sprite.Group()
+    all_walls = pygame.sprite.Group()
+    all_players = pygame.sprite.Group()
+    createWalls()
+    player = Player(1, 1, all_walls, layout)
+    all_sprites.add(player)
+    all_players.add(player)
+    plan = player.bfs([player.x, player.y], [8, 19])
+    print(plan)
+
+    # Main cycle
+    run = True
+    i = 1
+    while run:
+        CLOCK.tick(FPS)
+
+        # Process events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.move(dx = -1)
+                if event.key == pygame.K_RIGHT:
+                    player.move(dx = +1)
+                if event.key == pygame.K_UP:
+                    player.move(dy = -1)
+                if event.key == pygame.K_DOWN:
+                    player.move(dy = +1)
+        
+        player.move(dx = (plan[i][0] - player.x), dy = (plan[i][1] - player.y))        
+
+        # Update
+        all_sprites.update()
+
+        # Render
+        SCREEN.fill(WHITE)
+        all_sprites.draw(SCREEN)
+        draw_grid()
+        pygame.display.flip()
+
+        i += 1
+
+    pygame.quit()
+
+
+
+# W W W W W W W W W W W W W W W W W W W W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W W O W W W O W W W O W W O O W O O O W
+# W O O O O O O O O O O O O O O W O O O W
+# W O O O O O O O O O O O O O O W O O O W
+# W O O O O O O O O O O O O O O W O O O E
+# W O O O O W W O W W W W W O O W O O O W
+# W O O O O W W O W W W W W O O W O O O W
+# W O O O O O O O O O O O O O O W O O O W
+# W O O O O O O O O O O O O O O W O O O W
+# W O O O O O O O O O O O O O O W O O O W
+# W W W O W W W O W W W O W O O W O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W O O O W O O O W O O O W O O O O O O W
+# W W W W W W W W W W W W W W W W W W W W
---
+++ sprites.py
@@ -0,0 +1,87 @@
+# Sprites
+
+import pygame
+from settings import *
+
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, walls, layout):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.walls = walls
+        self.layout = layout
+
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx, dy):
+            self.x += dx
+            self.y += dy
+    
+    def collide_with_walls(self, dx=0, dy=0):
+        for wall in self.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        return False
+    
+    def update(self):
+        self.rect.x = self.x * TILESIZE 
+        self.rect.y = self.y * TILESIZE
+
+    def bfs(self, source, dest):        # [x,y] -> source | [x,y] -> destination
+        visited = [[0 for _ in range(len(self.layout))] for _ in range(len(self.layout))]
+        queue = []
+        path = []
+        prev = []
+        queue.append(source)
+        
+        visited[source[0]][source[1]] = 1
+        
+        row = [-1, 0, 0, 1]
+        col = [0, -1, 1, 0]
+        
+        while (len(queue) > 0):
+            cur = queue.pop(0)
+            if(cur == dest): break
+
+            for i in range(len(row)):
+                x = cur[0] + row[i]
+                y = cur[1] + col[i]
+
+                if (x < 0 or y < 0 or x >= len(self.layout) or y >= len(self.layout)): continue
+                if(self.layout[x][y] != 'W' and visited[x][y] == 0):
+                    visited[x][y] = 1
+                    list = []
+                    list.append(x)
+                    list.append(y)
+                    queue.append(list)
+
+                    p = []
+                    p.append(list)
+                    p.append(cur)
+                    prev.append(p)              # prev = [ [no, predecessor] ]
+
+        at = dest
+        while at != source:
+            path.append(at)
+            for i in range(len(prev)):
+                if(at == prev[i][0]): at = prev[i][1]
+        path.append(source)
+        path.reverse()
+        return path
+
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = self.x * TILESIZE 
+        self.rect.y = self.y * TILESIZE
